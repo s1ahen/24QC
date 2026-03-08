91c42b8285f453d86d781e639d7ec3b0be16b09f
s1ahen <potful_flak.0s@icloud.com>
2026-03-08 10:53:03 +0100

    Added system requirements

--- js/config.js
+++ js/config.js
@@ -8,15 +8,15 @@ const CONFIG = {
   SUPABASE_URL: 'https://xddjucvjyrouooycyege.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkZGp1Y3ZqeXJvdW9veWN5ZWdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NjA4OTUsImV4cCI6MjA4ODUzNjg5NX0.clJw9E_0Kacp-c9ou24bd3GIB841t1SdnmiPUWSOJqM',
   // --- Discord OAuth2 ---
-  DISCORD_CLIENT_ID: 'YOUR_DISCORD_CLIENT_ID',
-  DISCORD_REDIRECT_URI: 'YOUR_GITHUB_PAGES_URL/callback.html',
+DISCORD_CLIENT_ID: '1363053471728668792',
+DISCORD_REDIRECT_URI: 'https://s1ahen.github.io/24QC/callback.html',
   // e.g. 'https://atc24resources.github.io/24QuickCharts/callback.html'
 
   // --- Discord IDs allowed to review submissions ---
   // Add Discord user IDs (as strings) here
   REVIEWER_IDS: [
-    // 'DISCORD_ID_HERE',
-    // 'DISCORD_ID_HERE',
+    // '828709371201585153',
+    // '786262323517456404',
   ],
 
   // --- Storage ---
